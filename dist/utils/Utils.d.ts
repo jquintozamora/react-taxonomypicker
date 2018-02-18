3a692724e6fdb958e539bc508eee972b9d528249
Jose Quinto <quintorel@hotmail.com>
2018-02-18 18:01:16 +0000

    Updates

--- dist/utils/Utils.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Utils {
-    static getSiteCollectionUrl(): string;
-    static getCurrentAbsoluteSiteUrl(): string;
-    static getWebServerRelativeUrl(): string;
-    static getLayoutsPageUrl(libraryName: string): string;
-    static getAbsoluteDomainUrl(): string;
-}
